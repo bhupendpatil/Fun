50b614c7bf92987b6a923dd994bf70214469225a
Bhupend Patil <bhupendpatil@gmail.com>
2019-03-16 19:58:22 +0530

    auto files

---
+++ Mount&UmountEFI/Accessing.cmd
@@ -0,0 +1,10 @@
+rem it will set EFI partition as Y
+rem remember to edit this file if disk is not 0
+rem remember to edit this file if EFI partition is not 1 
+rem you can change letter to anything you want too..
+
+@echo off
+echo Keep assigning.txt in C:\
+pause
+
+diskpart /s C:\assigning.txt
---
+++ Mount&UmountEFI/Removing.cmd
@@ -0,0 +1,10 @@
+rem it will remove EFI partition as Y
+rem remember to edit this file if disk is not 0
+rem remember to edit this file if EFI partition is not 1 
+rem you can change letter to anything you want too..
+
+@echo off
+echo Keep removing.txt in C:\
+pause
+
+diskpart /s C:\removing.txt
