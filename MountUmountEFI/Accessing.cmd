3d9db87a83d4cd530e421d7b34716285dab61047
Bhupend Patil <bhupendpatil@gmail.com>
2019-03-17 11:17:02 +0530

    auto files

--- MountUmountEFI/Accessing.cmd
+++ MountUmountEFI/Accessing.cmd
@@ -1,6 +1,6 @@
 rem it will set EFI partition as Y
-rem remember to edit this file if disk is not 0
-rem remember to edit this file if EFI partition is not 1 
+rem remember to edit .txt file if disk is not 0
+rem remember to edit .txt file if EFI partition is not 1 
 rem you can change letter to anything you want too..
 
 @echo off
--- MountUmountEFI/Removing.cmd
+++ MountUmountEFI/Removing.cmd
@@ -1,7 +1,7 @@
 rem it will remove EFI partition as Y
-rem remember to edit this file if disk is not 0
-rem remember to edit this file if EFI partition is not 1 
-rem you can change letter to anything you want too..
+rem remember to edit .txt file if disk is not 0
+rem remember to edit .txt file if EFI partition is not 1 
+rem you can change to the one which is assigned to EFI partition.
 
 @echo off
 echo will run assigning.txt now
